8585083bcd5ef13ab3140c1de511bb445350594f
Brandon Duong <brandon.duong@outlook.com>
2020-07-31 00:54:56 -0400

    Added models, effects, level 3.
    Added models, effects, level 3.

---
+++ Assets/Bomb.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject explosionEffect;
+
+    bool exploded = false;
+    public float blastRadius = 5f;
+    public float explosionForce = 50f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (exploded == false && collision.collider.tag == "Player")
+        {
+            Debug.Log("Exploded");
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        exploded = true;
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach(Collider nearbyObject in nearbyObjects)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, blastRadius);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
